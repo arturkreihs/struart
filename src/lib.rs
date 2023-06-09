463106f87d5848ebb5c6cb1cd503ff6e4a9da1e9
Artur Kreihs <akreihs@ascs.pl>
2023-06-09 20:39:17 +0000

    inmuttable reference; better error handling;

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,37 @@
 use serial2::SerialPort;
 use thiserror::Error;
+use std::sync::RwLock;
 
 #[derive(Error, Debug)]
 pub enum StruartError {
-    #[error("IO")]
+    #[error(transparent)]
     IO(#[from] std::io::Error),
+
+    #[error("RwLock was Poisoned")]
+    RwLockPoisoned,
 }
 
 pub struct Struart {
     port: SerialPort,
-    buf: Vec<u8>,
+    buffer: RwLock<Vec<u8>>,
 }
 
 impl Default for Struart {
     fn default() -> Self {
-        Self::new("/dev/ttyUSB0", 115200)
+        Self::new("/dev/ttyUSB0", 115200).unwrap()
     }
 }
 
 impl Struart {
-    pub fn new(dev: &str, speed: u32) -> Self {
-        let mut port = SerialPort::open(dev, speed).unwrap();
-        port.set_read_timeout(core::time::Duration::from_millis(1000)).unwrap();
-        port.set_write_timeout(core::time::Duration::from_millis(1000)).unwrap();
+    pub fn new(dev: &str, speed: u32) -> Result<Self, StruartError> {
+        let mut port = SerialPort::open(dev, speed)?;
+        port.set_read_timeout(core::time::Duration::from_millis(100))?;
+        port.set_write_timeout(core::time::Duration::from_millis(1000))?;
 
-        Self {
+        Ok(Self {
             port,
-            buf: vec![],
-        }
+            buffer: RwLock::new(vec![]),
+        })
     }
 
     pub fn send(&self, text: &str) -> Result<(), StruartError> {
@@ -37,16 +41,16 @@ impl Struart {
         Ok(())
     }
 
-    pub fn read<F: Fn(&str)>(&mut self, cb: F) -> Result<(), StruartError> {
+    pub fn read<F: Fn(&str)>(&self, cb: F) -> Result<(), StruartError> {
         let mut buf = [0u8; 256];
         while let Ok(len) = self.port.read(&mut buf) {
             for byte in buf.iter().take(len) {
                 match byte {
                     0x0d => { // newline char
-                        cb(&String::from_utf8_lossy(&self.buf));
-                        self.buf.clear();
+                        cb(&String::from_utf8_lossy(&self.buffer.read().map_err(|_| StruartError::RwLockPoisoned)?));
+                        self.buffer.write().map_err(|_| StruartError::RwLockPoisoned)?.clear();
                     },
-                    0x20..=0x7e => self.buf.push(*byte), // printable char
+                    0x20..=0x7e => self.buffer.write().map_err(|_| StruartError::RwLockPoisoned)?.push(*byte), // printable char
                     _ => continue,
                 }
             }
